ad6e2216777464c7140762ae3bc9b8edfeea8293
James FitzGibbon <james@nadt.net>
2013-07-21 08:43:15 -0700

    sync instead of checkout to pick up changes

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['btrfs-snap']['dir'] = '/opt/btrfs-snap'
+default['btrfs-snap']['git_url'] = 'https://nadt.net/stash/scm/code/btrfs-snap.git'
+default['btrfs-snap']['git_ref'] = 'nadt'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'btrfs-snap'
+maintainer       'James FitzGibbon'
+maintainer_email 'james@nadt.net'
+license          'All rights reserved'
+description      'Installs btrfs-snap for Ubuntu and provides LWRPs to take snapshots'
+version          '0.1.4'
+supports         'ubuntu', '>= 12'
+depends          'accumulator'
