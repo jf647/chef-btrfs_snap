db5b62495066a05c4c60cd5fa1dce69bb4dfbfb6
James FitzGibbon <james.fitzgibbon@nsn.com>
2013-07-30 09:43:58 -0700

    fix scrub recipe; was still using old attribute name and passing parameters to cron resource incorrectly

--- recipes/scrub.rb
+++ recipes/scrub.rb
@@ -1,8 +1,8 @@
-if node['btrfs_snap']['enable_weekly_scrub']
-    cron "btrfs_weekly_scrub" do
+if node['btrfs_snap']['enable_periodic_scrub']
+    cron "btrfs_periodic_scrub" do |c|
         command "#{node['btrfs_snap']['btrfs_prog']} scrub start #{node['btrfs_snap']['rootmount']}"
-        node['btrfs_snap']['weekly_scrub_schedule'].each do |k, v|
-            k v
+        node['btrfs_snap']['periodic_scrub_schedule'].each do |k, v|
+            c.send(k, v)
         end
     end
 end
